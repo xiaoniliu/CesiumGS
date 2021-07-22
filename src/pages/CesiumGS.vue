8a61c49b76fc54e0368831bf38512295a45a5469
18057950097@163.com <18057950097@163.com>
2021-07-22 10:02:14 +0800

    :wrench: use preRender 实时更新infowindow数据

--- src/pages/CesiumGS.vue
+++ src/pages/CesiumGS.vue
@@ -2,6 +2,7 @@
   <div>
     <button class="edit-btn" @click="draw">绘图</button>
     <div id="cesiumContainer"></div>
+    <div class="info-window" v-show="showInfoWindow">InfoWindow</div>
   </div>
 </template>
 
@@ -15,7 +16,9 @@ export default {
     return {
       viewer: undefined,
       pointCoordinates: [],
-      pointCartesian3: []
+      pointCartesian3: [],
+      showInfoWindow: false,
+      tempUrl: 'https://sandcastle.cesium.com/'
     }
   },
   mounted () {
@@ -32,6 +35,36 @@ export default {
           heading: Cesium.Math.toRadians(90, 0),
           pitch: Cesium.Math.toRadians(-90),
           roll: 0.0
+        }
+      })
+      const point = {
+        show: true, // 是否展示
+        pixelSize: 20, // 点的大小
+        // heightReference:HeightReference.NONE,//相对高度
+        color: Cesium.Color.RED, // 颜色
+        outlineColor: Cesium.Color.SKYBLUE, // 边框颜色
+        outlineWidth: 5 // 边框宽度
+      }
+      viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(116.2, 39.56, 0),
+        point
+      })
+      this.viewer.screenSpaceEventHandler.setInputAction((movement) => {
+        const pick = viewer.scene.pick(movement.position)
+        if (pick && pick.id) {
+          this.showInfoWindow = true
+        }
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
+      viewer.scene.preRender.addEventListener(() => {
+        var position = Cesium.Cartesian3.fromDegrees(116.2, 39.56, 0)
+        var canvasPosition = viewer.scene.cartesianToCanvasCoordinates(
+          position,
+          new Cesium.Cartesian2()
+        )
+        if (this.showInfoWindow && Cesium.defined(canvasPosition)) {
+          const infoWindow = document.getElementsByClassName('info-window')[0]
+          infoWindow.style.left = canvasPosition.x - 50 + 'px'
+          infoWindow.style.top = canvasPosition.y - 100 + 'px'
         }
       })
     },
@@ -71,9 +104,11 @@ export default {
         if (!_pointCoordinates.length) {
           _pointCoordinates.push({ lng, lat, isClick: true })
           _pointCartesian3.push(cartesian3)
-          this.drawPolygon(new Cesium.CallbackProperty((time, result) => {
-            return new Cesium.PolygonHierarchy(this.pointCartesian3)
-          }, false))
+          this.drawPolygon(
+            new Cesium.CallbackProperty((time, result) => {
+              return new Cesium.PolygonHierarchy(this.pointCartesian3)
+            }, false)
+          )
         } else {
           const pointObject = { lng, lat, isClick: true }
           _pointCoordinates[_pointCoordinates.length - 1] = pointObject
@@ -96,9 +131,15 @@ export default {
           }
           this.pointCartesian3 = _pointCartesian3
           this.pointCoordinates = _pointCoordinates
-          this.viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE)
-          this.viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK)
-          this.viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.RIGHT_CLICK)
+          this.viewer.screenSpaceEventHandler.removeInputAction(
+            Cesium.ScreenSpaceEventType.MOUSE_MOVE
+          )
+          this.viewer.screenSpaceEventHandler.removeInputAction(
+            Cesium.ScreenSpaceEventType.LEFT_CLICK
+          )
+          this.viewer.screenSpaceEventHandler.removeInputAction(
+            Cesium.ScreenSpaceEventType.RIGHT_CLICK
+          )
         }
       }, Cesium.ScreenSpaceEventType.RIGHT_CLICK)
     },
@@ -128,17 +169,28 @@ export default {
       })
     }
   },
-  watch: {
-
-  }
+  watch: {}
 }
 </script>
 
 <style scoped>
-.edit-btn{
+.edit-btn {
   position: absolute;
   z-index: 999;
   top: 20px;
   left: 20px;
+}
+
+.info-window{
+  height: 80px;
+  width: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  background: #fff;
+  font-size: 24px;
+  position: absolute;
+  z-index: 10086;
 }
 </style>
